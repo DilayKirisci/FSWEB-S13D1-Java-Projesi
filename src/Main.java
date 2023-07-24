39f9872fb37b0c568335d01e7dcafdb10833ce43
Dilay Kirisci <isik.kirisci@gmail.com>
2023-07-24 21:26:18 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//    public static boolean shouldWakeUp(boolean isBarking, int clock ){
+//        if (clock > 23 || clock < 0){
+//            return false
+//        }
+//        if(!isBarking) {
+//            return false
+//        }
+//        if (clock < 20 || clock > 8){
+//            return true
+//        }
+//
+//    }
+//    public static hasTeen(){
+//
+//    }
+public static void main(String[] args) {
+    System.out.println("Hello world!");
+//    System.out.println(shouldWakeUp (true, 1);
+}
 }
